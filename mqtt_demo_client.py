a01c962bc868569691d49659f03d1a2f638da45e
Rii-Avent <rizkiihsanqilabi@gmail.com>
2022-06-25 09:10:56 +0700

    first commit

---
+++ mqtt_demo_client.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+
+# define static variable
+# broker = "localhost" # for local connection
+broker = "test.mosquitto.org"  # for online version
+port = 1883
+timeout = 60
+
+username = ''
+password = ''
+
+topic = "MANPASI/rizki"
+topic_greetings = "Manpasi/rizki/greetings"
+ 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+    # Subscribing in on_connect() - if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(topic,qos=2)
+ 
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    payload_decoded = msg.payload.decode('utf-8')
+    print("Hallo " +  payload_decoded + ", Selamat siang")
+    ret = client.publish(topic_greetings ,payload="Hallo " +  payload_decoded + ", Selamat siang", qos=2)
+        
+# Create an MQTT client and attach our routines to it.
+client = mqtt.Client("ALat0")
+client.username_pw_set(username=username,password=password)
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.connect(broker, port, timeout)
+
+client.loop_forever()
